6c9dc49c3e20bf35190a6efb4793b55b7770324d
Manuel Guariglia <manuelguariglia@gmail.com>
2021-11-04 16:01:54 +0100

    fix DOM Js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
-console.log('ciao');
+
+//creo l'array di cards
+
+const teamCards = [
+
+  {
+    cardImage : 'img/wayne-barnett-founder-ceo.jpg',
+    cardName : 'Wayne Barnett',
+    cardJob : 'Founder & CEO'
+  },
+
+  {
+    cardImage : 'img/angela-caroll-chief-editor.jpg',
+    cardName : 'Angela Caroll',
+    cardJob : 'Chief Editor'
+  }
+]
+
+
+
+console.log(teamCards)
+
+
+//inizializzo i contenuti html
+
+const cardContainer = document.querySelector('.team-container');
+
+const teamCard = document.createElement('div');
+teamCard.className = 'team-card';
+
+
+const cardImage = document.createElement('div');
+cardImage.className = 'card-image';
+cardImage.innerHTML = `
+
+<img
+src="${teamCards[1].cardImage}"
+alt="${teamCards[1].cardName}"
+/>
+
+`;
+
+const cardText = document.createElement('div');
+cardText.className = 'card-text';
+cardText.innerHTML = `
+<h3>${teamCards[1].cardName}</h3>
+<p>${teamCards[1].cardJob}</p>
+
+`;
+
+teamCard.append(cardImage);
+teamCard.append(cardText);
+cardContainer.append(teamCard);
+
+//estraggo i dati dalla card singola 
+//appendo i dati nel Dom
+
+
